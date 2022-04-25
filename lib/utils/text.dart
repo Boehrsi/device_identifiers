a4dc14a939871636b7bf98d90b6bc1edcd138622
Boehrsi <git@boehrsi.de>
2022-04-25 13:29:33 +0200

    fix: add missing backslash escaping in Dart

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -2,7 +2,7 @@ import '../targets/target_definition.dart';
 
 String escape(dynamic content, TargetDefinition target) {
   if (Type.dart == target.type) {
-    return content.toString().replaceAll("'", "\\\'");
+    return content.toString().replaceAll("\\", "\\\\").replaceAll("'", "\\\'");
   } else if (Type.kotlin == target.type) {
     return content.toString().replaceAll('\\', '\\\\').replaceAll('"', '\\\"');
   } else {
