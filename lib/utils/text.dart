59b5675bd1f48b88d05ad54da960c15bea26ef1e
Boehrsi <git@boehrsi.de>
2025-03-30 15:24:11 +0200

    fix: generation errors

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -2,9 +2,11 @@ import '../targets/target_definition.dart';
 
 String escape(dynamic content, TargetDefinition target) {
   if (Type.dart == target.type) {
-    return content.toString().replaceAll("\\", "\\\\").replaceAll("'", "\\'");
+    // ignore: unnecessary_string_escapes
+    return content.toString().replaceAll("\\", "\\\\").replaceAll("'", "\\\'");
   } else if (Type.kotlin == target.type) {
-    return content.toString().replaceAll('\\', '\\\\').replaceAll('"', '\\"');
+    // ignore: unnecessary_string_escapes
+    return content.toString().replaceAll('\\', '\\\\').replaceAll('"', '\\\"');
   } else {
     throw Exception("Unknown target");
   }
